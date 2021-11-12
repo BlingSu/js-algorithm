6eb62938b01ba0d3c361a0aa89bcfa5de6936472
ivanly <21978301@qq.com>
2021-11-12 14:54:22 +0800

    feat: 二叉树层序遍历 bfs

---
+++ src/array/bfs.js
@@ -0,0 +1,46 @@
+/**
+ * 二叉树的层序遍历
+ * */
+
+const root = {
+  val: "A",
+  left: {
+    val: "B",
+    left: {
+      val: "D"
+    },
+    right: {
+      val: "E"
+    }
+  },
+  right: {
+    val: "C",
+    right: {
+      val: "F"
+    }
+  }
+};
+
+
+function BFS (root) {
+  const queue = [];
+
+  queue.push(root);
+
+  while (queue.length) {
+    const top = queue[0];
+
+    console.log(top.val)
+    if(top.left) {
+      queue.push(top.left)
+    }
+
+    if (top.right) {
+      queue.push(top.right);
+    }
+
+    queue.shift();
+  }
+}
+
+BFS(root);
